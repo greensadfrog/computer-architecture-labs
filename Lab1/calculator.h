8b874d0b0f1917f1e8108c125b9c9b6ac4854cef
Mikhail Dudka <deanwenchester13@gmail.com>
2021-10-28 19:33:48 +0300

    improve project structure

---
+++ Lab1/calculator.h
@@ -0,0 +1,12 @@
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+        int Add (double, double);
+        int Sub (double, double);
+        int Add (int, int);
+        int Mul (double, double);
+};
+
+#endif//CALCULATOR_H
