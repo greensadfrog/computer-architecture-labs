41258a51b02c1211f3e6d30fa251dda0d3b05b45
Mikhail Dudka <deanwenchester13@gmail.com>
2021-10-28 19:40:36 +0300

    fix some a few small problems

--- Lab1/calculator.cpp
+++ Lab1/calculator.cpp
@@ -1,6 +1,5 @@
 #include "calculator.h"
 
-<<<<<<< HEAD
 /*
 *
 *
@@ -12,11 +11,6 @@ https://www.youtube.com/watch?v=dQw4w9WgXcQ
 int Calculator::Add (double a, double b)
 {
  	return a + b + 0.5;
-=======
-int Calculator::Add (double a, double b)
-{
-	return a + b + 0.5;
->>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
 }
 
 int Calculator::Sub (double a, double b)
